817339d06f671c22c769dbfb3189cdaeb07e0d7e
MallammaHR <mallumanish17@gmail.com>
2025-09-11 12:48:38 -0600

    master

--- incrementdecremnt.js
+++ incrementdecremnt.js
@@ -9,4 +9,20 @@ let n=m++;
 console.log(m);//-998
 console.log(n);//-999
 
-//p
+//pree-increment
+let p=2;
+let q=++p;
+console.log(p);//3
+console.log(q); //3
+
+//post-decrement
+let k=-89;
+let h=k--;
+console.log(k);//-90
+console.log(h);//-89
+
+//pre-decrement
+let v=2;
+let z=v--;
+console.log(v);//1
+console.log(z);//2
